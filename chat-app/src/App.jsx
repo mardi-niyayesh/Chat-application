efe40d6999e2d7b4dd5583765935297b888078d2
niyayesh <niyayeshammardii@gmail.com>
2025-11-09 16:06:14 +0330

    creat base rooot

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -1,10 +1,16 @@
+//components
+import Login from "./components/Login"
+import {Route,Routes} from "react-router-dom"
+
 
 function App() {
   
 
   return (
     <>
-    
+    <Routes>
+      <Route path="/" element={<Login/>}></Route>
+    </Routes>
     </>
   )
 }
