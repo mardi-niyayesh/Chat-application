b75d973f2958971c7afea8fc4ac4b67b03dee759
niyayesh <niyayeshammardii@gmail.com>
2025-11-09 16:47:43 +0330

    added className for style lofin page

--- chat-app/src/components/Login.jsx
+++ chat-app/src/components/Login.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
 //Icons
-import google from '../assents/google.svg'
-
+import google from "../assents/google.svg";
+//styles
+import styles from "../components/Login.module.css";
 
 const Login = () => {
-    return (
-        <div >
-            <div >
-                <h3>Welcom to ChatApp !</h3>
-            </div>
-            <div >
-                <img src={google} alt="logo google" /> sign with to google
-            </div>
+  return (
+    <>
+      <div className={styles.Loginpage}>
+        <div className={styles.Logincard}>
+          <h2>Welcom to ChatApp !</h2>
+          <div className={styles.button}>
+            <img src={google} alt="logo google" /> sign with to google
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Login;
